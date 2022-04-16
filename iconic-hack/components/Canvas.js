a414781ad2dd35c975be1852248dd18594ea25b5
Jagannathes <75128235+Jagannathes@users.noreply.github.com>
2022-04-17 00:55:08 +0530

    almost done

--- iconic-hack/components/Canvas.js
+++ iconic-hack/components/Canvas.js
@@ -1,24 +1,42 @@
 
-import React, { useRef, useEffect, useState } from 'react'
+import React, { useRef, useEffect, useState, useCallback } from 'react'
 import axios from 'axios';
+import { toPng } from 'html-to-image';
+
 
 
 const Canvas = ({text}) => {
+  
+  const ref = useRef(null)
+
+  const onButtonClick = useCallback(() => {
+    if (ref.current === null) {
+      return
+    }
+    toPng(ref.current, { cacheBust: true, })
+    .then((dataUrl) => {
+      const link = document.createElement('a')
+      link.download = 'my-image-name.png'
+      link.href = dataUrl
+      link.click()
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}, [ref])
   const BASEURL = 'http://localhost:3000';
   const canvasRef = useRef(null);
   const linkRef = useRef(null);
   const [dataURL, setDataUrl] = useState(null);
-  useEffect(() => {
-      const canvas = canvasRef.current;
-      const context = canvas.getContext('2d');
-      context.fillStyle = '#b2cfe9'
-      context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-      context.font = "30px Arial";
-      context.fillStyle = '#000000';
-      // context.fillText(props.text, 10, 50);
-      // setDataUrl(canvas.toDataURL("image/png"));
-      // console.log(dataURL);
-  }, []);
+  const [data,setData]=useState(null);
+
+  const ComponentToPrint = (props, ref) => (
+    <div ref={ref}>{data}</div>
+
+  );
+
+
+  
   function download() {
     // console.log("hello" + threadLink);
     // console.log(`${BASEURL}/api/tweet/${threadLink.split('/')[-1]}`);
@@ -27,22 +45,7 @@ const Canvas = ({text}) => {
     axios.get(`${BASEURL}/api/tweet/${_Id}`)
     .then(response => {
         // If request is good...
-        const tweetText = response.data.text.data.text
-        console.log(tweetText);
-        const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
-        context.fillStyle = '#b2cfe9'
-        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-        context.font = "30px Arial";
-        context.fillStyle = '#000000';
-        context.fillText(tweetText, 10, 50);
-        setDataUrl(canvas.toDataURL("image/png"));
-        console.log(dataURL);
-        const a = linkRef.current;
-        a.href = dataURL; //Image Base64 Goes here
-        a.download = "Image.png"; //File name Here
-        console.log(a);
-        a.click(); //Downloaded file
+       setData(response.data.text.data.text);
       })
     .catch((error) => {
         console.log('error ' + error);
@@ -52,9 +55,33 @@ const Canvas = ({text}) => {
   }
   return (
     <>
-      <a ref={linkRef} ></a>
-      <canvas ref={canvasRef}/>
-      <button onClick={download}>Download</button>
+     
+     
+      
+      <button onClick={download}>Go</button>
+      <div ref = {ref} style={{width:"100% !important"}}>
+        <div style = {{
+          flexGrow:1, 
+          display: 'flex',  
+          justifyContent: 'center', 
+          alignItems: 'center',
+         
+
+          }}>
+      <div style={{
+      width: '70%', 
+      height: '100%', 
+      backgroundColor: '#fff', 
+      border:"1px solid #000",
+      borderRadius:"30px",
+      padding:"10px",
+      wordBreak:"break-all",
+      fontSize:"40px",
+      }}>
+        {data}
+        </div></div>
+      </div>
+      {data?<button onClick={onButtonClick}>Download</button>:null}
     </>
   )
 }
